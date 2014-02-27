8c3d9273a6f186d4e91075961c482f8d31c91215
Shane Cavaliere <shane@shanecavaliere.com>
2014-02-27 01:50:15 -0700

    Added local environment db config to post create-project script

--- scripts/Easywpeasy/Installer.php
+++ scripts/Easywpeasy/Installer.php
@@ -16,6 +16,14 @@ class Installer {
     'NONCE_SALT'
   );
 
+  // Based on the default values already included in config/environments/local.php
+  public static $DB_CONFIG = array(
+    'Database Name' =>      'database_name',
+    'Database User' =>      'database_user',
+    'Database Password' =>  'database_password',
+    'Database Host' =>      'database_host'
+  );
+
   public static function addSalts(Event $event) {
     $root = dirname(dirname(__DIR__));
     $composer = $event->getComposer();
@@ -25,7 +33,7 @@ class Installer {
         if (!$io->isInteractive()) {
           $generate_salts = $composer->getConfig()->get('generate-salts');
         } else {
-          $generate_salts = $io->askConfirmation('<info>Generate salts in config/salts.php file? If not, you will have to edit this file yourself and add the auth key/salt constants. </info> [<comment>Y,n</comment>]? ', true);
+          $generate_salts = $io->askConfirmation('<info>Generate keys & salts in config/salts.php file? If not, you will have to edit this file yourself and add the key/salt constants. </info> [<comment>Y,n</comment>]? ', true);
         }
 
         if (!$generate_salts) {
@@ -39,7 +47,34 @@ class Installer {
         fwrite($salts_file, "<?php \n");
         fwrite($salts_file, implode($salts, "\n")); 
         
-        fclose($salts_file); 
+        fclose($salts_file);
+
+        if ($io->isInteractive()) {
+          $io->write('<info>Keys & salts successfully generated.</info>');
+        }
+    } else {
+      return 1;
+    }
+  }
+
+  public static function configureLocalEnvironment(Event $event) {
+    if ($io->isInteractive()) {
+      $root = dirname(dirname(__DIR__));
+      $composer = $event->getComposer();
+      $io = $event->getIO();
+      $local_env_file_path = "{$root}/config/environments/local.php";
+      $generate_local_env = $io->askConfirmation('<info>Would you like to enter the database connection information for your local environment now? </info> [<comment>Y,n</comment>]? ', true);
+      if($generate_local_env) {
+          foreach (self::$DB_CONFIG as $db_key => $db_value) {
+            $local_db_value = $io->ask('<info>'.$db_key.'</info>', $db_value);
+            $file_contents = file_get_contents($local_env_file_path);
+            $file_contents = str_replace($db_value, $local_db_value, $file_contents);
+            file_put_contents($local_env_file_path, $file_contents);
+          }
+          $io->write('<info>Local database connection info successfully added to config/environments/local.php.</info>');
+      } else {
+        return 1;
+      }
     } else {
       return 1;
     }
