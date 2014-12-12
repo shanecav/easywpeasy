fc02b4800b2cd24436ab67dcf50fada0bc72e989
Shane Cavaliere <shane@shanecavaliere.com>
2014-12-12 06:13:40 -0700

    updated local config's error handling to avoid issues with wp-sync-db

--- config/environments/local.php
+++ config/environments/local.php
@@ -1,12 +1,14 @@
 <?php
-define('DB_NAME', 			'database_name');
-define('DB_USER', 			'database_user');
-define('DB_PASSWORD', 		'database_password');
-define('DB_HOST', 			'database_host');
+define('DB_NAME', 			   'database_name');
+define('DB_USER', 			   'database_user');
+define('DB_PASSWORD', 	   'database_password');
+define('DB_HOST', 			   'database_host');
 
-define('WP_HOME', 			$wp_home);
-define('WP_SITEURL', 		$wp_siteurl);
+define('WP_HOME', 			   $wp_home);
+define('WP_SITEURL', 		   $wp_siteurl);
 
-define('SAVEQUERIES', 		true);
-define('WP_DEBUG', 			true);
-define('SCRIPT_DEBUG', 		true);
+define('SAVEQUERIES',      true);
+define('WP_DEBUG',         true);
+define('WP_DEBUG_LOG',     true);
+define('WP_DEBUG_DISPLAY', true);
+define('SCRIPT_DEBUG',     true);
